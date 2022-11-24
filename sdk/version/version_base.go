ed61bf0169f805401c39d6c7b99516a3b2a7c448
Sarah <sthompson@hashicorp.com>
2022-11-24 11:34:14 +0000

    Revert "add read func to version_base.go"
    This reverts commit 3f407eba294bb62f1a31098f1d8ca09ce7e9ee6a.

--- sdk/version/version_base.go
+++ sdk/version/version_base.go
@@ -1,11 +1,5 @@
 package version
 
-import (
-	"io"
-	"os"
-	"strings"
-)
-
 var (
 	// The git commit that was compiled. This will be filled in by the compiler.
 	GitCommit   string
@@ -17,16 +11,5 @@ var (
 	// Whether cgo is enabled or not; set at build time
 	CgoEnabled bool
 
-	Version = ReadVersion()
+	Version = ""
 )
-
-func ReadVersion() string {
-	f, err := os.Open("../../.release/VERSION")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-	b := new(strings.Builder)
-	io.Copy(b, f)
-	return b.String()
-}
